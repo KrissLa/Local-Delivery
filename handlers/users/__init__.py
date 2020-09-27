8de983ed4f8c22e11da2f556ed0dfea5f5f0e6a5
KrissLa <arsavit@gmail.com>
2020-09-27 13:34:05 +0300

    init

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,11 +1,12 @@
 from .cancel_order import dp
+from .admin import dp
 from .start import dp
 from .first import dp
 from .cart import dp
 from .commands import dp
 from .paginations import dp
 from .help import dp
-from .admin import dp
+
 from .restart_and_menu import dp
 from .couriers import dp
 from .sellers import dp
